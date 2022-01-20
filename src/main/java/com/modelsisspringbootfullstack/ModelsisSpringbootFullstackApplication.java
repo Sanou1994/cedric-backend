342e5bf57cc553fd8eb3ac55f8973973d3a1e53d
Sanou1994 <sanouarouna90@gmail.com>
2022-01-20 23:32:37 +0000

    ok

--- src/main/java/com/modelsisspringbootfullstack/ModelsisSpringbootFullstackApplication.java
+++ src/main/java/com/modelsisspringbootfullstack/ModelsisSpringbootFullstackApplication.java
@@ -1,19 +1,35 @@
 package com.modelsisspringbootfullstack;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
-public class ModelsisSpringbootFullstackApplication {
+import com.modelsisspringbootfullstack.dto.request.UserDtoRequest;
+import com.modelsisspringbootfullstack.dto.response.UserDtoResponse;
+import com.modelsisspringbootfullstack.iservice.IUserService;
 
+@SpringBootApplication
+public class ModelsisSpringbootFullstackApplication implements CommandLineRunner{
+    @Autowired
+	private IUserService iUserService;
 	public static void main(String[] args) {
 		SpringApplication.run(ModelsisSpringbootFullstackApplication.class, args);
 	}
 	@Bean
 	public BCryptPasswordEncoder getBCPE() {
 		return new BCryptPasswordEncoder();
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		UserDtoResponse user=iUserService.se_connecter("bollky.nana@gmail.com", "bollky.nana@gmail.com");
+		if(user == null) {
+			iUserService.login_up( new UserDtoRequest("NANA","bollky.nana@gmail.com","bollky.nana@gmail.com",
+					"bollky.nana@gmail.com",null,null));	
+		}
+		
 	}
     
 }
